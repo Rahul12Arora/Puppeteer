a0329c767b5c014a919bed7e675fcd599fad463b
Rahul Arora <rahul.012.arora@gmail.com>
2024-09-27 19:20:20 +0530

    Update index.js

--- FrontEnd/src/config/index.js
+++ FrontEnd/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // backEnd: "http://localhost:5003/"
-    backEnd: "https://puppeteer-backend.onrender.com"
+    backEnd: "https://puppeteer-backend.onrender.com/"
 }
 
 export default config;
